e19c74280f4f09eae0b730a86dde5c4c87801c69
itsmdzihad <mdzihad2244@gmail.com>
2026-07-27 02:13:13 +0600

    add: now create product is fully functional

--- src/App/modules/product/product.services.ts
+++ src/App/modules/product/product.services.ts
@@ -1,5 +1,25 @@
-const createProduct = (payload: any) => {
-  return payload;
+import { Prisma } from "../../../generated/prisma/client.js";
+import { prisma } from "../../config/db.js";
+
+const createProduct = async (payload: any) => {
+  const data = await prisma.product.create({
+    data: {
+      name: payload.name,
+      description: payload.description,
+      images: payload.images,
+      price: new Prisma.Decimal(payload.price),
+      quantity: Number(payload.quantity),
+      sizes: {
+        create: payload.sizes.map((size: any) => ({
+          name: size.name,
+          images: size.images,
+          quantity: Number(size.quantity),
+          price: new Prisma.Decimal(size.price),
+        })),
+      },
+    },
+  });
+  return data;
 };
 
 export const productService = {
