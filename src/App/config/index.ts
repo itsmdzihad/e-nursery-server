efe39c7249ab778c90fdd369cefe6ca4e7abd591
itsmdzihad <mdzihad2244@gmail.com>
2026-08-05 03:05:26 +0600

    update: add encryption on strong password

--- src/App/config/index.ts
+++ src/App/config/index.ts
@@ -5,5 +5,6 @@ dotEnv.config();
 const PORT = process.env.PORT;
 const DB_URL = process.env.DB_URL_DEV as string;
 const nodeEnv = process.env.NODE_ENV as string;
+const secret = process.env.SECRET as string;
 
-export { PORT, DB_URL, nodeEnv };
+export { PORT, DB_URL, nodeEnv, secret };
--- src/App/modules/auth/auth.services.ts
+++ src/App/modules/auth/auth.services.ts
@@ -1,30 +1,49 @@
+import httpStatus from "http-status";
+import bcrypt from "bcryptjs";
 import { prisma } from "../../config/db.js";
+import AppError from "../../errors/AppError.js";
 
-const registerUser = (payload: {
+const registerUser = async (payload: {
   name: string;
   email: string;
   password: string;
 }) => {
-  console.log();
-  const data = prisma.user.create({
+  const hashPass = await bcrypt.hash(payload.password, 10);
+  const data = await prisma.user.create({
     data: {
       name: payload.name,
       email: payload.email,
-      password: payload.password,
+      password: hashPass,
     },
   });
 
-  return data;
+  const { password, ...withOurPass } = data;
+
+  return withOurPass;
 };
 
 const loginUser = async (payload: { email: string; password: string }) => {
-  const result = await prisma.user.findFirst({
+  const findUser = await prisma.user.findFirst({
     where: {
       email: payload.email,
     },
   });
 
-  return result;
+  if (!findUser) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Please Register to login");
+  }
+
+  const matchPass = await bcrypt.compare(
+    payload.password,
+    findUser?.password as string,
+  );
+
+  if (!matchPass) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Wrong Email or Password");
+  }
+  const { password, ...withOurPass } = findUser;
+
+  return withOurPass;
 };
 
 export const authService = {
