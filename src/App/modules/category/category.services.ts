58fbec061bc0e6c3e50c7ab2dec3122a1f62089c
itsmdzihad <mdzihad2244@gmail.com>
2026-08-07 01:58:20 +0600

    feat: add category creation

--- src/App/modules/category/category.services.ts
+++ src/App/modules/category/category.services.ts
@@ -1,6 +1,54 @@
-const createCategory = () => {
-    
+import slugify from "slugify";
+
+import httpStatus from "http-status";
+import { prisma } from "../../config/db.js";
+import AppError from "../../errors/AppError.js";
+
+const createCategory = async (payload: {
+  name: string;
+  description?: string;
+  image?: string;
+  parentId?: string;
+}) => {
+  return await prisma.$transaction(async (tx) => {
+    if (payload.parentId) {
+      const parent = await tx.category.findUnique({
+        where: {
+          id: payload.parentId,
+        },
+      });
+
+      if (!parent) {
+        throw new AppError(httpStatus.NOT_FOUND, "Parent category not found");
+      }
+    }
+
+    const slug = slugify(payload.name, {
+      lower: true,
+      strict: true,
+      trim: true,
+    });
+
+    const isSlugExist = await tx.category.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (isSlugExist) {
+      throw new AppError(httpStatus.CONFLICT, "Category already exists");
+    }
+
+    return await tx.category.create({
+      data: {
+        ...payload,
+        slug,
+      },
+    });
+  });
 };
+
+
 const getAllCategories = () => {};
 const getSingleCategory = () => {};
 const updateCategory = () => {};
