51ebb636793687c178f0d36e0791f30cc9e743dd
itsmdzihad <mdzihad2244@gmail.com>
2026-08-07 01:48:59 +0600

    add: make all service function implement later

---
+++ src/App/modules/category/category.services.ts
@@ -0,0 +1,36 @@
+const createCategory = () => {};
+const getAllCategories = () => {};
+const getSingleCategory = () => {};
+const updateCategory = () => {};
+const deleteCategory = () => {};
+
+const getCategoryTree = () => {};
+const getRootCategories = () => {};
+const getChildCategories = () => {};
+const moveCategory = () => {};
+const getCategoryBySlug = () => {};
+const checkSlugAvailability = () => {};
+
+const getProductsByCategory = () => {};
+const getProductsWithSubCategories = () => {};
+
+const countProducts = () => {};
+const countChildren = () => {};
+
+export const categoryService = {
+  createCategory,
+  getAllCategories,
+  getSingleCategory,
+  updateCategory,
+  deleteCategory,
+  getCategoryTree,
+  getRootCategories,
+  getChildCategories,
+  moveCategory,
+  getCategoryBySlug,
+  checkSlugAvailability,
+  getProductsByCategory,
+  getProductsWithSubCategories,
+  countProducts,
+  countChildren,
+};
