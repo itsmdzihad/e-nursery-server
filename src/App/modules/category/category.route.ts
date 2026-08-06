ac2e502af6f3a48c4a7df6ec1c35db6bb792506d
itsmdzihad <mdzihad2244@gmail.com>
2026-08-07 03:04:19 +0600

    feat: add auth guard on category route

--- src/App/modules/category/category.route.ts
+++ src/App/modules/category/category.route.ts
@@ -1,17 +1,22 @@
 import { Router } from "express";
 import { categoryController } from "./category.controller.js";
+import auth from "../../middleware/auth.js";
+import { Role } from "../../../type/index.js";
 
 const categoryRoute = Router();
 
 // Create
-categoryRoute.post("/", categoryController.createCategory);
+categoryRoute.post("/", auth(Role.ADMIN), categoryController.createCategory);
 
 // Read
 categoryRoute.get("/", categoryController.getAllCategories);
 categoryRoute.get("/tree", categoryController.getCategoryTree);
 categoryRoute.get("/roots", categoryController.getRootCategories);
 categoryRoute.get("/slug/:slug", categoryController.getCategoryBySlug);
-categoryRoute.get("/slug/check", categoryController.checkSlugAvailability);
+categoryRoute.get(
+  "/slug/check/:slug",
+  categoryController.checkSlugAvailability,
+);
 
 categoryRoute.get("/:categoryId", categoryController.getSingleCategory);
 categoryRoute.get(
@@ -36,10 +41,23 @@ categoryRoute.get(
 );
 
 // Update
-categoryRoute.patch("/:categoryId", categoryController.updateCategory);
-categoryRoute.patch("/:categoryId/move", categoryController.moveCategory);
+categoryRoute.patch(
+  "/:categoryId",
+  auth(Role.ADMIN),
+  categoryController.updateCategory,
+);
+
+categoryRoute.patch(
+  "/:categoryId/move",
+  auth(Role.ADMIN),
+  categoryController.moveCategory,
+);
 
 // Delete
-categoryRoute.delete("/:categoryId", categoryController.deleteCategory);
+categoryRoute.delete(
+  "/:categoryId",
+  auth(Role.ADMIN),
+  categoryController.deleteCategory,
+);
 
 export default categoryRoute;
