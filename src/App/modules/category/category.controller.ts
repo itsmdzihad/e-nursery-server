1bb0474c00b431a88effde4c5b329b60f676a4fa
itsmdzihad <mdzihad2244@gmail.com>
2026-08-07 04:08:47 +0600

    feat: all category conroller implement done

--- src/App/modules/category/category.controller.ts
+++ src/App/modules/category/category.controller.ts
@@ -45,40 +45,181 @@ const getSingleCategory = catchAsync(
   },
 );
 const updateCategory = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.updateCategory(
+      req.params.categoryId as string,
+      req.body,
+    );
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category Updated Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const deleteCategory = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.deleteCategory(
+      req.params.categoryId as string,
+    );
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category Deleted Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const getCategoryTree = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.getCategoryTree();
+    sendRes({
+      res,
+      success: true,
+      message: "Category fetch Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const getRootCategories = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.getRootCategories();
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category fetch Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const getChildCategories = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.getChildCategories(
+      req.params.categoryId as string,
+    );
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category fetch Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const moveCategory = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.moveCategory(
+      req.params.categoryId as string,
+      req.body.parentId,
+    );
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category fetch Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const getCategoryBySlug = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.getCategoryBySlug(
+      req.params.slug as string,
+    );
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category fetch Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const checkSlugAvailability = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.checkSlugAvailability(
+      req.params.slug as string,
+    );
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category fetch Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const getProductsByCategory = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.getProductsByCategory(
+      req.params.categoryId as string,
+    );
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category fetch Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const getProductsWithSubCategories = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.getProductsWithSubCategories(
+      req.params.categoryId as string,
+    );
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category fetch Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const countProducts = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.countProducts(
+      req.params.categoryId as string,
+    );
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category fetch Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 const countChildren = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await categoryService.countChildren(
+      req.params.categoryId as string,
+    );
+
+    sendRes({
+      res,
+      success: true,
+      message: "Category fetch Successfully",
+      statusCode: 200,
+      data: result,
+    });
+  },
 );
 
 export const categoryController = {
