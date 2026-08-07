97a92fa37d8804f20be6fb9b2fd60f4ee513045f
itsmdzihad <mdzihad2244@gmail.com>
2026-08-07 15:37:28 +0600

    feat: all address services are made  implement later

---
+++ src/App/modules/address/address.services.ts
@@ -0,0 +1,26 @@
+const createAddress = async () => {};
+
+const getAllAddresses = async () => {};
+
+const getMyAddresses = async () => {};
+
+const getSingleAddress = async () => {};
+
+const updateAddress = async () => {};
+
+const deleteAddress = async () => {};
+
+const setDefaultAddress = async () => {};
+
+const getDefaultAddress = async () => {};
+
+export const addressService = {
+  createAddress,
+  getAllAddresses,
+  getMyAddresses,
+  getSingleAddress,
+  updateAddress,
+  deleteAddress,
+  setDefaultAddress,
+  getDefaultAddress,
+};
