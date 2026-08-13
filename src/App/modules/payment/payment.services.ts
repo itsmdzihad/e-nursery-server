cc789582719eaa86c05c81b5fcbb772a098332fa
itsmdzihad <mdzihad2244@gmail.com>
2026-08-14 03:36:17 +0600

    feat: add all payment services function and will implement later

---
+++ src/App/modules/payment/payment.services.ts
@@ -0,0 +1,47 @@
+const createPayment = async () => {};
+
+const processPayment = async () => {};
+
+const verifyPayment = async () => {};
+
+const handlePaymentSuccess = async () => {};
+
+const handlePaymentFailure = async () => {};
+
+const handlePaymentWebhook = async () => {};
+
+const updatePaymentStatus = async () => {};
+
+const getPaymentById = async () => {};
+
+const getPaymentByOrderId = async () => {};
+
+const getUserPayments = async () => {};
+
+const getAllPayments = async () => {};
+
+const refundPayment = async () => {};
+
+const getRefundByPaymentId = async () => {};
+
+const cancelPayment = async () => {};
+
+const getPaymentHistory = async () => {};
+
+export const paymentService = {
+  createPayment,
+  processPayment,
+  verifyPayment,
+  handlePaymentSuccess,
+  handlePaymentFailure,
+  handlePaymentWebhook,
+  updatePaymentStatus,
+  getPaymentById,
+  getPaymentByOrderId,
+  getUserPayments,
+  getAllPayments,
+  refundPayment,
+  getRefundByPaymentId,
+  cancelPayment,
+  getPaymentHistory,
+};
