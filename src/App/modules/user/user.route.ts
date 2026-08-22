de93e91fdfbead5a095671f009ae490dae1fd8c3
itsmdzihad <mdzihad2244@gmail.com>
2026-08-22 22:41:55 +0600

    feat: all user route has been init and implement later

---
+++ src/App/modules/user/user.route.ts
@@ -0,0 +1,53 @@
+import { Router } from "express";
+
+import auth from "../../middleware/auth.js";
+import { Role } from "../../../type/index.js";
+import { userController } from "./user.controller.js";
+
+const userRoute = Router();
+
+// Customer
+
+userRoute.get(
+  "/me",
+  auth(Role.CUSTOMER, Role.ADMIN),
+  userController.getMyProfile,
+);
+
+userRoute.patch(
+  "/me",
+  auth(Role.CUSTOMER, Role.ADMIN),
+  userController.updateMyProfile,
+);
+
+userRoute.patch(
+  "/me/password",
+  auth(Role.CUSTOMER, Role.ADMIN),
+  userController.changePassword,
+);
+
+// Admin
+
+userRoute.get("/", auth(Role.ADMIN), userController.getAllUsers);
+
+userRoute.get("/email", auth(Role.ADMIN), userController.getUserByEmail);
+
+userRoute.get("/:userId", auth(Role.ADMIN), userController.getUserById);
+
+userRoute.patch("/:userId", auth(Role.ADMIN), userController.updateUser);
+
+userRoute.patch(
+  "/:userId/role",
+  auth(Role.ADMIN),
+  userController.updateUserRole,
+);
+
+userRoute.patch(
+  "/:userId/verification",
+  auth(Role.ADMIN),
+  userController.updateUserVerification,
+);
+
+userRoute.delete("/:userId", auth(Role.ADMIN), userController.deleteUser);
+
+export default userRoute;
