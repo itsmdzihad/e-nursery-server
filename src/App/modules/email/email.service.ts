9c2e99fe4b01367d50cd90ef9383ae7e63ff31a7
itsmdzihad <mdzihad2244@gmail.com>
2026-09-07 08:24:14 +0600

    feat: setup email service function on email module

---
+++ src/App/modules/email/email.service.ts
@@ -0,0 +1,20 @@
+const sendEmail = async () => {};
+
+const sendOtpEmail = async () => {};
+
+const sendWelcomeEmail = async () => {};
+
+const sendOrderConfirmationEmail = async () => {};
+
+const sendPaymentConfirmationEmail = async () => {};
+
+const sendChangePassOtpEmail = async () => {};
+
+export const emailService = {
+  sendEmail,
+  sendOtpEmail,
+  sendWelcomeEmail,
+  sendOrderConfirmationEmail,
+  sendPaymentConfirmationEmail,
+  sendChangePassOtpEmail,
+};
